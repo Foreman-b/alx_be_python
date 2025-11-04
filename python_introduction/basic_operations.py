5292471aabca76f3a6b1bb0150668a1ed110e6d2
Foreman-b <hamzat4life@gmail.com>
2025-11-04 12:54:00 -0800

    Addition, Subtraction and Multiplication of number1 and number2

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+print("Addition of", number1, "and", number2, "is", result)
+
+number1 = 10
+number2 = 5
+result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", result)
+
+number1 = 10
+number2 = 5
+result = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", result)
