eb0c445f7fc043a0ceb563cf05328337e9a9b5d6
Foreman-b <hamzat4life@gmail.com>
2025-11-13 12:38:05 +0100

    Add Personal Reminder Script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,8 +2,12 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match (priority, time_bound):
-    case ("high", "yes"):
-        print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-    case ("low", "no"):
-        print(f"Note: {task} is a low priority task. Consider completing it when you have free time.")
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+    case "low":
+        if time_bound == "no":
+            print(f"Note: {task} is a low priority task. Consider completing it when you have free time.")
+    case _:
+        print("Invalid priorit Entered.")
