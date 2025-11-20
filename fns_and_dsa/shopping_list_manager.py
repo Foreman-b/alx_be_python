bfba8ccaac384872f055337a7dfa3270cc99b70a
Foreman-b <hamzat4life@gmail.com>
2025-11-20 15:41:31 +0100

    Add Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to remove: ").strip()      # Prompt for and add an item
+            item = input("Enter item to add: ").strip()      # Prompt for and add an item
             shopping_list.append(item)
             print(f"{item} has been added to the list")
     
