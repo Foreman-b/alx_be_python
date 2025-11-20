7198974e7c79efea8be25a7116868509683e22b8
Foreman-b <hamzat4life@gmail.com>
2025-11-20 17:21:04 +0100

    Add Explore datetime Module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formartted_current_date = current_date.strftime("%y-%m-%d %H:%M:%S")
     print(f"Current date and time: {formartted_current_date}")
     
@@ -11,7 +11,7 @@ def display_current_datetime():
         print("Error: Enter Valid valid Number")
         return
     
-    time_delta = datetime.timedelta(days=number_of_days)
+    time_delta = timedelta(days=number_of_days)
     future_date = current_date + time_delta
     formatted_future_date = future_date.strftime("%Y-%m-%d")
     print(f"Future date: {formatted_future_date}")
